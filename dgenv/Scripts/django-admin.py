14a2150cae82ce2f4989acbae4c6f86195042ea5
surajdb01 <surajdb01@gmail.com>
2021-04-12 16:10:52 +0530

    My blog, first commit

---
+++ dgenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\suraj.kumar.dubey\pythonprojects\djangogirls\dgenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
