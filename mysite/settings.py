700cc84acec73bd171ffc192339a2db4147ac190
surajdb01 <surajdb01@gmail.com>
2021-04-13 16:00:10 +0530

    passing data to templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,9 +87,6 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
